553df47059f5f48181769f3a0a3d732fa5c17c83
Noah Pope <npope@tutanota.com>
2025-03-12 21:08:36 -0500

    Initial Commit

---
+++ Milestone6-CountryFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone6-CountryFacts
+//
+//  Created by Noah Pope on 3/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Milestone6-CountryFactsTests/Milestone6_CountryFactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone6_CountryFactsTests.swift
+//  Milestone6-CountryFactsTests
+//
+//  Created by Noah Pope on 3/12/25.
+//
+
+import Testing
+@testable import Milestone6_CountryFacts
+
+struct Milestone6_CountryFactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
