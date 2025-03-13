8b916811eeb3594375996f03a8fb19af1988cb69
Noah Pope <npope@tutanota.com>
2025-03-13 12:06:08 -0500

    editing comments

--- Milestone6-CountryFacts/Screens/DetailVC.swift
+++ Milestone6-CountryFacts/Screens/DetailVC.swift
@@ -10,18 +10,18 @@ import UIKit
 class DetailVC: UITableViewController
 {
     var countryItem: CountryItem
-    
-    //-------------------------------------//
-    // MARK: INITS & DEINITS
-    
-    /**this calls the super (UITableViewController)'s init**/
+
+    /**
+     this 1st designated init is resp. for calling the super (UITableViewController)'s init after setting countryItem
+     the 2nd required init is for the storyboard
+     **/
+        
     init(withCountryItem countryItem: CountryItem)
     {
         self.countryItem = countryItem
         super.init(nibName: nil, bundle: nil)
     }
     
-    /**this is for the storyboard**/
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
     deinit { print("deinitializing DetailVC") }
